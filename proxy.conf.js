240afe23a466a5f097e59b7e9673d064d042b43e
Dvwnmark <xcmark141@gmail.com>
2024-10-19 11:18:23 +0700

    update proxy conf

--- proxy.conf.js
+++ proxy.conf.js
@@ -15,7 +15,6 @@ const PROXY_CONFIG = [
       "/api",
     ],
     target: "https://toeicute-38c2b32a7c77.herokuapp.com/",
-    secure: false,
     "changeOrigin": true,
     "logLevel": "debug",
     headers: {host: 'toeicute-38c2b32a7c77.herokuapp.com'},
