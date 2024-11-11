658decc6a79a41d03ad062a24c4a4327f68b7425
Dvwnmark <xcmark141@gmail.com>
2024-11-11 23:44:19 +0700

    upgrade angular 17

--- src/app/admin/users/user-activity/user-activity.component.ts
+++ src/app/admin/users/user-activity/user-activity.component.ts
@@ -1,18 +1,15 @@
 import {Component, OnInit} from '@angular/core';
-import {BsModalService} from "ngx-bootstrap/modal";
 import {HttpClient} from "@angular/common/http";
-import {NzModalService} from "ng-zorro-antd/modal";
 import {NgxSpinnerService} from "ngx-spinner";
-import {TranslateService} from "@ngx-translate/core";
-import {ToastrService} from "ngx-toastr";
 import {CONSTANT} from "../../../common/constant";
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-user-activity',
   templateUrl: './user-activity.component.html',
   styleUrls: ['./user-activity.component.scss']
 })
-export class UserActivityComponent implements OnInit{
+export class UserActivityComponent implements OnInit {
   title: string = 'Activity User';
   currentPage: string = 'Activity';
   listAction: any = [];
@@ -25,24 +22,27 @@ export class UserActivityComponent implements OnInit{
     type: 'ALL'
   };
 
-  constructor(
-    private http: HttpClient,
-    private modal: NzModalService,
-    private spinner: NgxSpinnerService,
-    private  translate: TranslateService,
-    private toast: ToastrService
-  ) {
+  constructor(private http: HttpClient,
+              private spinner: NgxSpinnerService,
+              private translate: TranslateService) {
+    this.translate.setDefaultLang('en_US');
+    this.translate.use('en_US');
   }
+
   ngOnInit(): void {
     this.getLisActionUser();
   }
+
   getLisActionUser() {
-    this.http.get(`/api/admin/user/activity?page=${this.params.page-1}&size=${this.params.size}&type=${this.params.type}`)
+    this.spinner.show().then();
+    this.http.get(`/api/admin/user/activity?page=${this.params.page - 1}&size=${this.params.size}&type=${this.params.type}`)
       .subscribe((res: any) => {
+        this.spinner.hide().then();
         this.listAction = res.content;
         this.totalElements = res.totalElements;
       });
   }
+
   changePage(event: number) {
     this.params.page = event;
     this.getLisActionUser();
@@ -53,15 +53,26 @@ export class UserActivityComponent implements OnInit{
     this.params.page = 1;
     this.getLisActionUser();
   }
-  isLastElement(index: number, array: any[]): boolean {
-    return index === array.length - 1;
+
+  getJsonFromData(json: any) {
+    const obj = JSON.parse(json);
+    if (obj) {
+      return {
+        fullName: obj[0],
+        phone: obj[1],
+        address: obj[2],
+      };
+    }
+    return {};
   }
-  parsedOldData(oldData: string): string[] {
-    return JSON.parse(oldData);
+
+  getImage(oldData: any) {
+    if (oldData) {
+      const json = JSON.parse(oldData);
+      if (json) {
+        return json[json.length - 1];
+      }
+    }
+    return '';
   }
-  parsedNewData(newData: string): string[] {
-    return JSON.parse(newData);
-  }
-  protected readonly JSON = JSON;
-  protected readonly length = length;
 }
--- src/app/common/profile.service.ts
+++ src/app/common/profile.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
+import {Profile} from './model/Profile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-  currentUser: any;
-  paramUser: any = {
+  currentUser: Profile = new Profile();
+  paramUser: Profile = {
     fullName: '',
     email: '',
     avatar: '/assets/images/default-avatar.jpg',
@@ -28,20 +29,22 @@ export class ProfileService {
       .subscribe((res: any) => {
         if (res?.success) {
           this.isLogin = true;
-          const profile = {
-            avatar: res?.data?.avatar,
-            email: res?.data?.email,
-            fullName: res?.data?.fullName,
-            userId: res?.data?.userId,
+          const p: Profile = {
+            fullName: res.data.fullName,
+            email: res.data.email,
+            avatar: res.data.avatar,
+            password: '',
+            phone: res.data.phone,
+            address: res.data.address,
+            userId: res.data._id,
           };
-          this.currentUser = profile;
-          this.paramUser = {
-            ...this.paramUser,
-            ...profile,
-          };
+          this.currentUser = p;
+          this.paramUser = JSON.parse(JSON.stringify(p));
         } else {
           this.isLogin = false;
         }
       });
   }
 }
+
+
